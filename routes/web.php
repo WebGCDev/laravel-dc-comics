266707ed2643d7f5cf2caaa6676572ed86eec4f2
WebGCDev <gaetano.chi@gmail.com>
2024-02-01 18:23:06 +0100

    Add:  [RESOURCE] Comic Controller Create & Import in Web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
-
+use App\Http\Controllers\Admin\ComicController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,5 @@ use App\Http\Controllers\Admin\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::resource('comics', ComicController::class);
 
